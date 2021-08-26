594ccf71895020549d91e4841919051319d6ebc0
web-bruk <84834043+web-bruk@users.noreply.github.com>
2021-08-26 14:19:11 -0400

    update

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// $(document).ready(function(){
+// //     if($(window).width()< 768){
+// //     $(".footer-links-wrapper").addClass("saterdayclass");
+// // }else {
+// //     $(".footer-links-wrapper").removeClass("saterdayclass");
+// // }
+
+// //     $(window).on("resize",function (){
+// //         if($(window).width()< 768){
+// //             $(".footer-links-wrapper").addClass("saterdayclass");
+// //             //$(".footer_links_wrapper li").hide();
+// //         }else {
+// //             $(".footer-links-wrapper").removeClass("saterdayclass");
+// //             $(".footer-links-wrapper ul").show();
+// //             // $(".footer_links_wrapper li").show();
+// //         }
+// //         }
+// //     )
+// // var el = $(".navbar navbar-toggleable-sm navbar-expand-md");
+// // var el = $(".navbar-toggler")
+// var el = $(".navbar-nav");
+// // $(".navbar-toggler" ).click(function(){
+// //     el.slideToggle();
+// // });
+
+
+//     $(document).on("click", ".navbar-toggler ",
+//     function (){
+//         el.slideToggle();
+//         // $(this).slideToggle("li");
+//         // $(this).toggleClass("iconRotator");
+//     })
+// })
+//     // var el = $("#listOfFruits");
+//     //         $( ".fruits-menu" ).click(function(){
+//     //             el.toggle();
+//     //         });
